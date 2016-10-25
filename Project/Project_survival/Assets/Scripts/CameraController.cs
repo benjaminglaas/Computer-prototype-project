d24840d99fd3204cf6f7ebbeb8ee7929407bce3d
Benjamin Glaas <benjamin@glaas.dk>
2016-10-25 14:38:56 +0200

    Added player and rotation

--- Project/Project_survival/Assets/Scripts/CameraController.cs
+++ Project/Project_survival/Assets/Scripts/CameraController.cs
@@ -4,21 +4,52 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
 
 	public float speed = 10.0f;
-	float angle = 180.0f;
+	public float smooth = 1f;
+
+
+	float angle = 90.0f;
+	int orientation = 0;
+	Vector3[] gravityDirectionList = new Vector3[4]{new Vector3 (0, -1, 0),new Vector3(1,0,0),new Vector3(0,1,0),new Vector3(-1,0,1)}; 
+	Quaternion targetRotation; 
+	Vector3 gravityDirection;
+
+
+
 	// Use this for initialization
+	//Vector3 gravityDirection;
 	void Start () {
-	
+		targetRotation = transform.rotation;
+		gravityDirection = gravityDirectionList [0];
+		Physics.gravity = gravityDirection;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.D)) {
-			print ("D key was pressed");
-			transform.RotateAround (Vector3.zero, new Vector3 (1,1,0),angle);
+			if (orientation >= 3) {
+				orientation = 0;
+			}
+			else{
+			orientation += 1;
+			};
+			targetRotation *= Quaternion.AngleAxis(angle, new Vector3 (0, 0, 1));
+			gravityDirection = gravityDirectionList [orientation];
+			Physics.gravity = gravityDirection;
+			print (Physics.gravity);
 		}
 		if (Input.GetKeyDown (KeyCode.A)) {
-			print ("A key was pressed");
-			transform.RotateAround (Vector3.zero, new Vector3 (1,1,0),-angle);		
+			if (orientation <= 0) {
+				orientation = 3;
+			}
+			else{
+				orientation -= 1;
+			};
+			targetRotation *= Quaternion.AngleAxis(-angle, new Vector3 (0, 0, 1));
+			gravityDirection = gravityDirectionList [orientation];
+			Physics.gravity = gravityDirection;
+			print (Physics.gravity);
+
 		}
+		transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, 10 * smooth * Time.deltaTime);
 	}
 }
