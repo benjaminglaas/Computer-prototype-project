6d5d618ab614eb280eca84e999e01b568c4ed0c7
Benjamin Glaas <benjamin@glaas.dk>
2016-11-08 17:19:35 +0100

    Player movement fine

---
+++ Project/Project_survival/Assets/Scripts/BottomCollision.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BottomCollision : MonoBehaviour {
+
+	Rigidbody2D rb;
+	float gravityScale;
+	GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+		gravityScale = PlayerController.gravityScale;
+		player = GameObject.FindGameObjectWithTag ("Player");
+		rb = player.GetComponent<Rigidbody2D>(); 
+		rb.gravityScale = gravityScale;
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		
+		if (other.gameObject.tag == "Ground") {	
+			PlayerController.jumping = false;
+			rb.gravityScale = 0.0f;
+			rb.velocity = Vector3.zero;
+
+		}
+
+	}
+	void OnTriggerStay2D(Collider2D other){
+		if (other.gameObject.tag == "Ground") {	
+			PlayerController.jumping = false;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		if (other.gameObject.tag == "Ground") {
+			PlayerController.jumping = true;
+			rb.gravityScale = gravityScale;
+		}
+	}	
+}
---
+++ Project/Project_survival/Assets/Scripts/SideCollision.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SideCollision : MonoBehaviour {
+
+
+	Rigidbody2D rb;
+	float gravityScale;
+	GameObject player;
+	Vector3[] directionJumpList;
+
+	// Use this for initialization
+	void Start () {
+		
+		gravityScale = PlayerController.gravityScale;
+		player = GameObject.FindGameObjectWithTag ("Player");
+		rb = player.GetComponent<Rigidbody2D>(); 
+		rb.gravityScale = gravityScale;
+		directionJumpList = PlayerController.directionJumpList;
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+
+		if (other.gameObject.tag == "Ground") {	
+			PlayerController.jumping = true;
+			rb.gravityScale = gravityScale;
+			rb.AddForce (-directionJumpList [PlayerController.orientationPlayer] * Time.deltaTime);
+		}
+
+	}
+	void OnTriggerStay2D(Collider2D other){
+		
+		if (other.gameObject.tag == "Ground") {	
+			PlayerController.jumping = true;
+			rb.AddForce (-directionJumpList [PlayerController.orientationPlayer] * Time.deltaTime);
+
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		
+		if (other.gameObject.tag == "Ground") {
+			PlayerController.jumping = true;
+			rb.gravityScale = gravityScale;
+			rb.AddForce (-directionJumpList [PlayerController.orientationPlayer] * Time.deltaTime);
+		}
+	}	
+
+	void FixedUpdate(){
+	}
+}
